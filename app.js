f23499c58d884762888757c0f572b7446cdb5d47
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-29 16:35:22 -0700

    Require mongoose and dotenv packages. Set up db connection

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 // DB connection
+mongoose.connect(process.env.DEV_DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("connected to db");
+});
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
