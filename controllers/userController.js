4b9a64a0a20a1f40be11cecbade8731b444e5895
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-01 20:23:28 -0700

    Initial commit. Create userController with signUp()

---
+++ controllers/userController.js
@@ -0,0 +1,99 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const { validateSignup } = require("../utils/validation");
+
+const signUp = function signUp(req, res) {
+    // 1) Validate email, username, and password
+
+    const { username, email, password } = req.body;
+    const validationRes = validateSignup.validate({
+        username,
+        email,
+        password
+    });
+
+    if (
+        validationRes.error &&
+        validationRes.error.details &&
+        validationRes.error.details.length > 0
+    ) {
+        var errorMessages = validationRes.error.details.map(function(errorObj) {
+            return errorObj.message;
+        });
+
+        return res.json({
+            username,
+            email,
+            success: false,
+            data: {
+                errorMessages
+            }
+        });
+    }
+
+    // 2) Check if both email and username are available
+    User.findOne({ email })
+        .then(function(userEmail) {
+            if (userEmail) {
+                throw {
+                    message: "email already exists"
+                };
+            }
+
+            return User.findOne({ username });
+        })
+        .then(function(username) {
+            if (username) {
+                throw {
+                    message: "username already exists"
+                };
+            }
+
+            return bcrypt.genSalt(10);
+        })
+        .then(function(salt) {
+            // 3) Hash password
+
+            if (!salt) {
+                throw {
+                    message: "failed to generate salt"
+                };
+            }
+
+            return bcrypt.hash(password, salt);
+        })
+        .then(function(hash) {
+            if (!hash) {
+                throw {
+                    message: "failed to hash password"
+                };
+            }
+
+            var newUser = new User({
+                username,
+                email,
+                password: hash,
+                signUpDate: new Date().getTime()
+            });
+
+            // 4) Save info to DB
+            return newUser.save();
+        })
+        .then(function(data) {
+            // 5) Return token
+            return res.json({
+                success: true,
+                data
+            });
+        })
+        .catch(function(err) {
+            return res.json({
+                success: false,
+                data: err
+            });
+        });
+};
+
+module.exports = {
+    signUp
+};
