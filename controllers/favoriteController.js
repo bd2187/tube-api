be7333cde2f957a6ca47220caa9f834ecc6078b5
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-03 21:17:49 -0700

    Initial commit. Create fetchFavoriteVideos()

---
+++ controllers/favoriteController.js
@@ -0,0 +1,8 @@
+const User = require("../models/userModel");
+const Favorite = require("../models/favoriteModel");
+
+const fetchFavoriteVideos = function(req, res) {};
+
+module.exports = {
+    fetchFavoriteVideos
+};
