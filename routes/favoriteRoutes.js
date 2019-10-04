fd98246031bbba1f8608aa1e6f6dac2dd233aba7
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-03 21:14:59 -0700

    Initial commit. Create POST routes for /all and /add/video. Create delete route for /remove/video

---
+++ routes/favoriteRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+router.get("/all", (req, res) => {});
+
+router.post("/add/video", (req, res) => {});
+
+router.delete("/remove/video", (req, res) => {});
+
+module.exports = router;
