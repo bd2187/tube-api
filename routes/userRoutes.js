e9a5eaf5970d2ed06b3d8b1b8f456b4645dc8ff0
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-03 21:14:00 -0700

    Remove passport

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
 const { signUp, signIn } = require("../controllers/userController");
 
 router.post("/signup", signUp);
 
 router.post("/signin", signIn);
-
-// router.post("/signin", passport.authenticate("local"), function(req, res) {
-//     // 1) Validate email/username, and password
-
-//     // 2) Find email/username in DB
-//     // - validate password
-
-//     // 3) Return token
-
-//     return res.json({ test: "test", data: req.body });
-// });
 
 router.post("/delete", function(req, res) {
     // 1) Validate email, username, and password
