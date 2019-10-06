ce034b558ddbc03998d0f946fd4b6449189a43cc
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-05 18:51:27 -0700

    Add logic to fetchFavoriteVideos() so that it searches a USER by ID and returns their favorites

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,7 +1,35 @@
 const User = require("../models/userModel");
 const Favorite = require("../models/favoriteModel");
 
-const fetchFavoriteVideos = function(req, res) {};
+const fetchFavoriteVideos = function(req, res) {
+    const { userID } = req.params;
+
+    User.findById(userID)
+        .then(function(user) {
+            if (!user) {
+                throw {
+                    message: "user not found"
+                };
+            }
+
+            const { favorites, username, email, _id: id } = user;
+            return res.json({
+                success: true,
+                data: {
+                    id,
+                    username,
+                    email,
+                    favorites
+                }
+            });
+        })
+        .catch(function(err) {
+            return res.json({
+                success: false,
+                data: err
+            });
+        });
+};
 
 module.exports = {
     fetchFavoriteVideos
