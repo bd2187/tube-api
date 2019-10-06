1cdf7249dadfce0bb2179305e744c7f80d51b6a6
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-06 14:09:34 -0700

    Use passport's LocalStrategy to verfiy users during the login process.

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,63 @@
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/userModel");
+
+passport.use(
+    "login",
+    new LocalStrategy(
+        { usernameField: "email", passwordField: "password", session: false },
+        function(email, password, done) {
+            User.findOne({ email }, function(err, user) {
+                if (err) {
+                    return done(err);
+                }
+
+                if (!user) {
+                    return done(null, false, {
+                        message: "email not found"
+                    });
+                } else {
+                    bcrypt
+                        .compare(password, user.password)
+                        .then(function(isPasswordValid) {
+                            if (!isPasswordValid) {
+                                return done(null, false, {
+                                    message: "incorrect password"
+                                });
+                            }
+
+                            return done(null, user);
+                        });
+                }
+            }).catch(function(err) {
+                return done(err);
+            });
+        }
+    )
+);
+
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = "secret";
-opts.issuer = "accounts.examplesoft.com";
-opts.audience = "yoursite.net";
+opts.secretOrKey = "123secret";
 
-module.exports = function(passport) {
-    passport.use(
-        new JwtStrategy(opts, function(jwt_payload, done) {
-            User.findOne({ id: jwt_payload.sub }, function(err, user) {
-                if (err) {
-                    return done(err, false);
-                }
-                if (user) {
-                    return done(null, user);
-                } else {
-                    return done(null, false);
-                    // or you could create a new account
-                }
-            });
-        })
-    );
-};
+passport.use(
+    "jwt",
+    new JwtStrategy(opts, function(jwt_payload, done) {
+        User.findOne({ _id: jwt_payload.userID }, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+        });
+    })
+);
+
+module.exports = passport;
