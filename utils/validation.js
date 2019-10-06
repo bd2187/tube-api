3948d06089be99be358f927b73d25c5855708cb4
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-06 12:04:47 -0700

    Add schema for favoriteVideo

--- utils/validation.js
+++ utils/validation.js
@@ -4,17 +4,33 @@ const validateSignup = Joi.object({
     username: Joi.string()
         .alphanum()
         .min(7)
-        .max(30),
+        .max(30)
+        .required(),
 
-    email: Joi.string().email(),
+    email: Joi.string()
+        .email()
+        .required(),
 
-    password: Joi.string().pattern(/^[a-zA-Z0-9]{7,30}$/)
+    password: Joi.string()
+        .pattern(/^[a-zA-Z0-9]{7,30}$/)
+        .required()
 });
 
 const validateSignin = Joi.object({
-    email: Joi.string().email()
+    email: Joi.string()
+        .email()
+        .required()
+});
+
+const validateFavoriteVideo = Joi.object({
+    thumbnail: Joi.string().required(),
+    title: Joi.string().required(),
+    videoID: Joi.string().required(),
+    dateAdded: Joi.date()
+        .timestamp()
+        .required()
 });
 
 module.exports.validateSignup = validateSignup;
-
 module.exports.validateSignin = validateSignin;
+module.exports.validateFavoriteVideo = validateFavoriteVideo;
