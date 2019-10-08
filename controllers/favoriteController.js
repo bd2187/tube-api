6a8564f0ebb8612c0c7216bd794f16d86c71444a
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-07 20:27:32 -0700

    Create generatePassportErr fn

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -2,6 +2,15 @@ const User = require("../models/userModel");
 const passport = require("passport");
 const { validateFavoriteVideo } = require("../utils/validation");
 
+const generatePassportErr = function(message) {
+    return {
+        success: false,
+        data: {
+            message
+        }
+    };
+};
+
 /**
  *  Returns  user's favorites
  */
@@ -70,21 +79,15 @@ const addFavoriteVideo = function(req, res, next) {
 
     passport.authenticate("jwt", { session: false }, function(err, user, info) {
         if (err) {
-            return res.json({
-                success: false,
-                data: {
-                    message: "there was an issue with favoriting this video"
-                }
-            });
+            return res.json(
+                generatePassportErr(
+                    "there was an issue with favoriting this video"
+                )
+            );
         }
 
         if (info) {
-            return res.json({
-                success: false,
-                data: {
-                    message: info.message
-                }
-            });
+            return res.json(generatePassportErr(info.message));
         }
 
         const { _id: userID } = user;
@@ -136,7 +139,72 @@ const addFavoriteVideo = function(req, res, next) {
     })(req, res, next);
 };
 
+/**
+ * Removes a favorite video from the user's favorites array.
+ * 1) Validate incoming videoID to ensure that it matches the schema
+ * 2) Find the user in the DB and filter the video based on its from
+ * the user's favorites array
+ */
+const removeFavoriteVideo = function(req, res, next) {
+    var { videoID } = req.body;
+
+    // Todo: validate videoID
+
+    passport.authenticate("jwt", { session: false }, function(err, user, info) {
+        if (err) {
+            return res.json(
+                generatePassportErr(
+                    "there was an issue with favoriting this video"
+                )
+            );
+        }
+
+        if (info) {
+            return res.json(generatePassportErr(info.message));
+        }
+
+        const { _id: userID } = user;
+
+        User.findById(userID)
+            .then(function(user) {
+                if (!user) {
+                    throw {
+                        message: "user not found"
+                    };
+                }
+
+                const updatedFavorites = user.favorites.filter(function(
+                    favorite
+                ) {
+                    return favorite.videoID !== videoID;
+                });
+
+                return User.findOneAndUpdate(
+                    { _id: userID },
+                    { favorites: updatedFavorites },
+                    { new: true }
+                );
+            })
+            .then(function(data) {
+                return res.json({
+                    success: true,
+                    data: {
+                        message: "successfully removed favorite",
+                        data: data.favorites
+                    }
+                });
+            })
+            .catch(function(err) {
+                return res.json({
+                    success: false,
+                    data: err
+                });
+            });
+    })(req, res, next);
+};
+
 module.exports = {
     fetchFavoriteVideos,
-    addFavoriteVideo
+    addFavoriteVideo,
+    removeFavoriteVideo
 };
